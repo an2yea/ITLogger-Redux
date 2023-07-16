30add3993d65aec64acd4b83de802aabcb8dba44
Ananya <its.gupta.ananya@gmail.com>
2023-07-16 12:59:45 +0530

    Search Bar

--- it_logger/src/App.js
+++ it_logger/src/App.js
@@ -1,11 +1,20 @@
+import React, {Fragment, useEffect} from 'react'
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
+import SearchBar from './Components/layout/SearchBar';
 const App = () => {
+  // Initialise all materialise UI components in a single command instead of separate
+  useEffect(() => {
+    M.AutoInit();
+  })
+
   return (
-    <div className="App">
-      My App 
-    </div>
-  );
+    <Fragment>
+    <SearchBar /> 
+    </Fragment>
+  )
 }
 
 export default App;
